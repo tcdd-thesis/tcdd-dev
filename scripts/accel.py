4b5038c545683b74bcb84f32094732e11c19514a
JoshuaKCA <arcilla.joshuakyle@ue.edu.ph>
2026-02-20 02:23:04 +0800

    New code

--- scripts/accel.py
+++ scripts/accel.py
@@ -1,39 +1,33 @@
+# DONATION LINK: https://buymeacoffee.com/mmshilleh
+
 import time
 from mpu9250_jmdev.registers import *
 from mpu9250_jmdev.mpu_9250 import MPU9250
 
-# Initialize the MPU9250
-# Note: GFS (Gyro Full Scale), AFS (Accel Full Scale)
+# Create an MPU9250 instance
 mpu = MPU9250(
     address_ak=AK8963_ADDRESS,
-    address_mpu_master=MPU9050_ADDRESS_68, # Default I2C address
+    address_mpu_master=MPU9050_ADDRESS_68,  # In case the MPU9250 is connected to another I2C device
+    address_mpu_slave=None,
     bus=1,
-    gfs=GFS_250, 
-    afs=AFS_2G, 
-    mfs=AK8963_BIT_16, 
-    mode=AK8963_MODE_C100HZ
-)
+    gfs=GFS_1000,
+    afs=AFS_8G,
+    mfs=AK8963_BIT_16,
+    mode=AK8963_MODE_C100HZ)
 
-# Configure the sensor with the settings above
+# Configure the MPU9250
 mpu.configure()
 
-print("| {:^10} | {:^10} | {:^10} |".format("X", "Y", "Z"))
-print("-" * 40)
+while True:
+    # Read the accelerometer, gyroscope, and magnetometer values
+    accel_data = mpu.readAccelerometerMaster()
+    gyro_data = mpu.readGyroscopeMaster()
+    mag_data = mpu.readMagnetometerMaster()
 
-try:
-    while True:
-        # Read Accelerometer data
-        # Returns a list: [x, y, z]
-        accel_data = mpu.readAccelerometerMaster()
-        
-        x = accel_data[0]
-        y = accel_data[1]
-        z = accel_data[2]
+    # Print the sensor values
+    print("Accelerometer:", accel_data)
+    print("Gyroscope:", gyro_data)
+    print("Magnetometer:", mag_data)
 
-        # Print formatted data
-        print(f"| {x:10.4f} | {y:10.4f} | {z:10.4f} |", end="\r")
-        
-        time.sleep(0.1) # 10Hz refresh rate
-
-except KeyboardInterrupt:
-    print("\n\nStopped by user.")
+    # Wait for 1 second before the next reading
+    time.sleep(1)
